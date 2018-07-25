b222b6e30d742a2f91bbab36b7a2cea3de43b425
Robert Lemke <robert@flownative.com>
2018-07-25 19:22:59 +0200

    Update beach-cli.rb

--- Formula/beach-cli.rb
+++ Formula/beach-cli.rb
@@ -1,8 +1,7 @@
 class BeachCli < Formula
   desc "Command line tool for Flownative Beach"
   homepage "https://www.flownative.com/beach"
-  url "https://storage.googleapis.com/cli-tool.beach.flownative.cloud/beach.phar"
-  version 1.0.0
+  url "https://storage.googleapis.com/cli-tool.beach.flownative.cloud/beach-0.1.3.phar"
   sha256 "251404e6dad16e2d6fe7b70412d3f97f79e8878202974187f0d53915d10693e6"
 
   bottle :unneeded
