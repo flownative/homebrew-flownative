27cf5dace7b4e182161f6140db9c76f174ffab1c
Robert Lemke <robert@flownative.com>
2018-07-26 11:40:41 +0200

    Upgrade Beach CLI to 0.2.0

--- Formula/beach-cli.rb
+++ Formula/beach-cli.rb
@@ -1,12 +1,12 @@
 class BeachCli < Formula
   desc "Command line tool for Flownative Beach"
   homepage "https://www.flownative.com/beach"
-  url "https://storage.googleapis.com/cli-tool.beach.flownative.cloud/beach-0.1.3.phar"
-  sha256 "251404e6dad16e2d6fe7b70412d3f97f79e8878202974187f0d53915d10693e6"
+  url "https://storage.googleapis.com/cli-tool.beach.flownative.cloud/beach-0.2.0.phar"
+  sha256 "9a0bfb984fd7fd0ae202cdbb19034926be7bb3c01a671247eca238950dfe5b75"
 
   bottle :unneeded
 
   def install
-    bin.install "beach-0.1.3.phar" => "beach"
+    bin.install "beach-0.2.0.phar" => "beach"
   end
 end
