f46bc52b52a5614f3dc954d27f3a3dcf01c3961f
GitLab CI <gitlab@flownative.com>
2018-08-28 21:23:34 +0000

    Update beach.phar to 0.2.10

--- Formula/beach-cli.rb
+++ Formula/beach-cli.rb
@@ -4,13 +4,13 @@
 class BeachCli < Formula
   desc "Command line tool for Flownative Beach"
   homepage "https://www.flownative.com/beach"
-  url "https://storage.googleapis.com/cli-tool.beach.flownative.cloud/beach-0.2.9.phar"
-  sha256 "30724d81106ffe65da0399c0d9c22490a939ab466f1b9a5d899178c0c99fabb1"
+  url "https://storage.googleapis.com/cli-tool.beach.flownative.cloud/beach-0.2.10.phar"
+  sha256 "192d6457cc139f954d09a130c8b78c95714d814e7c0d31e6e57aa28dc0974884"
 
   bottle :unneeded
 
   def install
-    bin.install "beach-0.2.9.phar" => "beach"
-    system "beach" "localbeach:prepare" "#{bin}/" "#{var}/localbeach"
+    bin.install "beach-0.2.10.phar" => "beach"
+    system "beach", "localbeach:prepare", "#{bin}/", "#{var}/localbeach"
   end
 end
