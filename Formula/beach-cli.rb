44dcd80e509c68aea57b1f78c64e39c49621799a
GitLab CI <gitlab@flownative.com>
2018-11-02 09:06:00 +0000

    Update beach.phar to 0.9.2

--- Formula/beach-cli.rb
+++ Formula/beach-cli.rb
@@ -4,16 +4,16 @@
 class BeachCli < Formula
   desc "Command line tool for Flownative Beach"
   homepage "https://www.flownative.com/beach"
-  url "https://storage.googleapis.com/cli-tool.beach.flownative.cloud/beach-0.9.1.phar"
-  sha256 "3666542bea7bbfd34994daebe6d854db1df6b189b27adac4c9a4ddf384f13c0a"
+  url "https://storage.googleapis.com/cli-tool.beach.flownative.cloud/beach-0.9.2.phar"
+  sha256 "9c62e4ff5a77b7aba40d6a994de909dd945bd8ee9674779d94821d093365f02a"
 
   head "https://storage.googleapis.com/cli-tool.beach.flownative.cloud/beach-master.phar"
 
   bottle :unneeded
 
   def install
-    system "php", buildpath/"beach-0.9.1.phar", "localbeach:prepare", "#{lib}/beach-cli/localbeach", "~/Library/Application Support/Flownative/Local Beach/MariaDB"
-    bin.install "beach-0.9.1.phar" => "beach"
+    system "php", buildpath/"beach-0.9.2.phar", "localbeach:prepare", "#{lib}/beach-cli/localbeach", "~/Library/Application Support/Flownative/Local Beach/MariaDB"
+    bin.install "beach-0.9.2.phar" => "beach"
   end
 
   plist_options :manual => "beach localbeach:start"
